1c8808f8baaca1742d812014faf15d48eab30955
Gerry1402 <gerard.vello@gmail.com>
2024-04-23 00:30:05 +0200

    escoba

--- ESCOBA/Escoba.py
+++ ESCOBA/Escoba.py
@@ -1,23 +1,66 @@
+import numpy
+from itertools import chain, combinations
 
-Baraja={}
-Numeros=['As','2','3','4','5','6','7','Sota','Caballo','Rey']
-Palos=['oro','copa','espada','basto']
-equivalencias={'As':'1','Sota':'8','Caballo':'9','Rey':'10'}
-for i in Numeros:
+def combinaciones(lista):
+    s = list(lista)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+def Baraja_Cartas (Numeros, Palos,Equivalencias):
+    Baraja={}
+    Cartas=[]
     for j in Palos:
-        Carta=i+' de '+j+'s'
-        if i in equivalencias:
-            i=equivalencias[i]
-        Valor=[int(i),j]
-        Baraja[Carta]=Valor
-contrincantes=input('Especificar numero de contrincantes\n')
-print ('Cartas en la mesa. Especificar individualmente. Primero numero y luego palo')
-Mesa=[]
-for i in range (1,5):
-    numero=input('Seleccionar '+', '.join(Numeros)+'\n')
-    palo=input('Seleccionar '+', '.join(Palos)+'\n')
-    Mesa.append([numero, palo])
+        for i in Numeros:
+            Carta=i+' de '+j+'s'
+            Cartas.append(Carta)
+            if i in Equivalencias:
+                i=Equivalencias[i]
+            Valor=[int(i),j]
+            Baraja[Carta]=Valor
+    return Baraja, Cartas
+
+def transformar_numeros(Cartas,Baraja):
+    numeros=[]
+    palos=[]
+    for carta in Cartas:
+        numeros.append(Baraja[carta][0])
+        palos.append(Baraja[carta][1])
+    return numeros, palos
+
+def Repartir_Mesa(Restantes,Numero_Mesa):
+    Mesa=numpy.random.choice(Restantes, Numero_Mesa, False)
+    for carta in Mesa:
+        Restantes.remove(carta)
+    return Mesa
+
+def Repartir_Jugador(Restantes,Numero_Jugador):
+    Jugador=numpy.random.choice(Restantes, Numero_Jugador, False)
+    for carta in Jugador:
+        Restantes.remove(carta)
+    return Jugador
+
+def comprobar_mesa(Mesa):
+    treinta=False
+    quince=False
+    numeros, palos = transformar_numeros(Mesa, Baraja)
+    for combinacion in combinaciones(numeros):
+        if sum(combinacion)==15:
+            quince=True
+        elif sum(combinacion)==30:
+            treinta=True
+        if treinta and quince:
+            print('Escoba de mano')
+            Mesa=[]
+    return Mesa
 
 #Holaa
 ## ESCOBA ##
-
+Numeros=['As','2','3','4','5','6','7','Sota','Caballo','Rey']
+Palos=['oro','copa','espada','basto']
+Equivalencias={'As':'1','Sota':'8','Caballo':'9','Rey':'10'}
+Baraja, Restantes=Baraja_Cartas (Numeros, Palos,Equivalencias)
+Mesa=Repartir_Mesa(Restantes,4)
+Jugador=Repartir_Jugador(Restantes,3)
+Mesa=comprobar_mesa(Mesa)
+Contrincantes={'Contrincante_1':[], 'Contrincante_2':[], 'Contrincante_3':[]}
+for contrincante in Contrincantes.keys():
+    Contrincantes[contrincante]=Repartir_Jugador(Restantes,3)
